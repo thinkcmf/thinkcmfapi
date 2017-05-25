b1673f97ef7cd80a3d43a2a7657c9a458bd2fc38
wuwu <15093565100@163.com>
2017-05-25 23:39:30 +0800

    幻灯片注释

--- api/user/model/SlideModel.php
+++ api/user/model/SlideModel.php
@@ -17,29 +17,7 @@ use think\Model;
 
 class SlideModel extends Model
 {
-    /**
-     * [SlideItemModel 一对一关联模型 关联分类下的幻灯片]
-     * @Author:   wuwu<15093565100@163.com>
-     * @DateTime: 2017-05-25T23:30:27+0800
-     * @since:    1.0
-     */
-	protected function SlideItemModel()
-    {
-        return $this->hasOne('SlideItemModel');
-    }
-
-	/**
-	 * [SlideList 幻灯片获取]
-	 * @Author:   wuwu<15093565100@163.com>
-	 * @DateTime: 2017-05-25T20:52:27+0800
-	 * @since:    1.0
-	 */
-    public function SlideList($map)
-    {
-    	$data = $this -> relation('slide_item_model') -> field(true) -> where($map) ->select();
-    	return $data;
-    }
-
+    
     /**
      * [base 全局查询范围status=1显示状态]
      * @Author:   wuwu<15093565100@163.com>
@@ -50,5 +28,29 @@ class SlideModel extends Model
     {
         $query -> where('status',1);
     }
+
+    /**
+     * [SlideItemModel 一对一关联模型 关联分类下的幻灯片]
+     * @Author:   wuwu<15093565100@163.com>
+     * @DateTime: 2017-05-25T23:30:27+0800
+     * @since:    1.0
+     */
+    protected function SlideItemModel()
+    {
+        return $this->hasOne('SlideItemModel');
+    }
+
+    /**
+     * [SlideList 幻灯片获取]
+     * @Author:   wuwu<15093565100@163.com>
+     * @DateTime: 2017-05-25T20:52:27+0800
+     * @since:    1.0
+     */
+    public function SlideList($map)
+    {
+        $data = $this -> relation('slide_item_model') -> field(true) -> where($map) ->select();
+        return $data;
+    }
+
 }
 
