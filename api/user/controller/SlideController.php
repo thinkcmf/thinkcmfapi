0b62bbd61419a3d83bbb31b64913a50ea4ba398b
wuwu <15093565100@163.com>
2017-05-25 23:23:12 +0800

    幻灯片获取

---
+++ api/user/controller/SlideController.php
@@ -0,0 +1,44 @@
+<?php
+// +----------------------------------------------------------------------
+// | 文件说明：幻灯片  
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017 http://www.wuwuseo.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: wuwu <15093565100@163.com>
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Date: 2017-说明-日
+// +----------------------------------------------------------------------
+namespace api\user\controller;
+use api\user\model\SlideModel;
+use cmf\controller\RestUserBaseController;
+
+class SlideController extends RestUserBaseController
+{
+    /**
+     * [getSlide 获取幻灯片]
+     * @Author:   wuwu<15093565100@163.com>
+     * @DateTime: 2017-05-25T20:48:53+0800
+     * @since:    1.0
+     * @return    [array_json] [获取幻灯片]
+     */
+    public function getSlide()
+    {
+        //slide为空或不存在抛出异常
+        if(!$this -> request -> has('slide') || empty($this -> request -> param('slide'))){
+           $this -> error(['code' => 0, 'msg' => '缺少slide参数']);
+        }
+        $map['id'] = $this -> request -> param('slide');
+    	$obj = new SlideModel();
+    	$data = $obj -> SlideList($map);
+
+        //剔除分类状态隐藏 剔除分类下显示数据为空
+        if($data -> isEmpty() || empty($data->toArray()[0]['slide_item_model'])){
+            $this -> error(['code' => 0, 'msg' => '该组幻灯片显示数据为空']);
+        }
+
+        $this -> success("该组幻灯片获取成功!", $data);
+    }
+
+}
---
+++ api/user/model/SlideModel.php
@@ -0,0 +1,48 @@
+<?php
+// +----------------------------------------------------------------------
+// | 文件说明：用户模型
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017 http://www.wuwuseo.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: wuwu <15093565100@163.com>
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Date: 2017-5-24
+// +----------------------------------------------------------------------
+
+namespace api\user\model;
+
+use think\Model;
+
+class SlideModel extends Model
+{
+	protected function SlideItemModel()
+    {
+        return $this->hasOne('SlideItemModel');
+    }
+
+	/**
+	 * [SlideList 幻灯片获取]
+	 * @Author:   wuwu<15093565100@163.com>
+	 * @DateTime: 2017-05-25T20:52:27+0800
+	 * @since:    1.0
+	 */
+    public function SlideList($map)
+    {
+    	$data = $this -> relation('slide_item_model') -> field(true) -> where($map) ->select();
+    	return $data;
+    }
+
+    /**
+     * [base 全局查询范围status=1显示状态]
+     * @Author:   wuwu<15093565100@163.com>
+     * @DateTime: 2017-05-25T21:54:03+0800
+     * @since:    1.0
+     */
+    protected function base($query)
+    {
+        $query -> where('status',1);
+    }
+}
+
