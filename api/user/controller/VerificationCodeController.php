dad98f4ca570fe7c913af02bc628bdf9c549167c
zxxjjforever <zxxjjforever@163.com>
2017-07-06 11:48:38 +0800

    短信验证码发送插件化

--- api/user/controller/VerificationCodeController.php
+++ api/user/controller/VerificationCodeController.php
@@ -47,16 +47,47 @@ class VerificationCodeController extends RestBaseController
 
         if ($accountType == 'email') {
 
-            //TODO 实现邮箱验证码发送
-            cmf_verification_code_log($data['username'],'666666');
+            $emailTemplate = cmf_get_option('email_template_verification_code');
+
+            $user     = cmf_get_current_user();
+            $username = empty($user['user_nickname']) ? $user['user_login'] : $user['user_nickname'];
+
+            $message = htmlspecialchars_decode($emailTemplate['template']);
+            $message = $this->display($message, ['code' => $code, 'username' => $username]);
+            $subject = empty($emailTemplate['subject']) ? 'ThinkCMF验证码' : $emailTemplate['subject'];
+            $result  = cmf_send_email($data['username'], $subject, $message);
+
+            if (empty($result['error'])) {
+                cmf_verification_code_log($data['username'], $code);
+                $this->success("验证码已经发送成功!");
+            } else {
+                $this->error("邮箱验证码发送失败:" . $result['message']);
+            }
 
         } else if ($accountType == 'mobile') {
 
-            //TODO 实现手机验证码发送
-            cmf_verification_code_log($data['username'],'666666');
+            $param  = ['mobile' => $data['username'], 'code' => $code];
+            $result = hook_one("send_mobile_verification_code", $param);
+
+            if ($result !== false && !empty($result['error'])) {
+                $this->error($result['message']);
+            }
+
+            if ($result === false) {
+                $this->error('未安装验证码发送插件,请联系管理员!');
+            }
+
+            cmf_verification_code_log($data['username'], $code);
+
+            if (!empty($result['message'])) {
+                $this->success($result['message']);
+            } else {
+                $this->success('验证码已经发送成功!');
+            }
+
         }
 
-        $this->success("验证码已经发送成功!您的验证码默认是666666");
+
     }
 
 }
