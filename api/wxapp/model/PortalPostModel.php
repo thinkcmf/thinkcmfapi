861ac903c2fc4b781200c9ab45d4d8f7c1789492
wuwu <15093565100@163.com>
2017-07-14 22:44:22 +0800

    小程序准备工作

--- api/wxapp/model/PortalPostModel.php
+++ api/wxapp/model/PortalPostModel.php
@@ -1,6 +1,6 @@
 <?php
 // +----------------------------------------------------------------------
-// | 文件说明：用户-幻灯片
+// | 文件说明：文章分类与文章关系
 // +----------------------------------------------------------------------
 // | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
 // +----------------------------------------------------------------------
@@ -8,24 +8,26 @@
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Date: 2017-5-25
+// | Date: 2017-7-14
 // +----------------------------------------------------------------------
 
-namespace api\user\model;
+namespace api\wxapp\model;
 
 use think\Model;
 
-class SlideItemModel extends Model
+class PortalCategoryPostModel extends Model
 {
-    /**
-     * [base 全局查询范围status=1显示状态]
-     * @Author:   wuwu<15093565100@163.com>
-     * @DateTime: 2017-05-25T21:54:03+0800
-     * @since:    1.0
-     */
-    protected function base($query)
-    {
-        $query -> where('status',1);
-    }
+    
+	/**
+	 * [base 全局查询范围status=1显示状态]
+	 * @Author:   wuwu<15093565100@163.com>
+	 * @DateTime: 2017-07-14T21:54:03+0800
+	 * @since:    1.0
+	 */
+	protected function base($query)
+	{
+		$query -> where('post_status',1);
+	}
+
 }
 
