e8b6f55f88bfc31174cc345fb14672906e7c1ed7
pl125 <xskjs888@163.com>
2017-07-16 01:30:03 +0800

    排版

--- api/portal/model/PortalCategoryModel.php
+++ api/portal/model/PortalCategoryModel.php
@@ -1,73 +1,73 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: pl125 <xskjs888@163.com>
-// +----------------------------------------------------------------------
+	// +----------------------------------------------------------------------
+	// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+	// +----------------------------------------------------------------------
+	// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+	// +----------------------------------------------------------------------
+	// | Author: pl125 <xskjs888@163.com>
+	// +----------------------------------------------------------------------
 
-namespace api\portal\model;
+	namespace api\portal\model;
 
-use api\common\model\ParamsFilterModel;
+	use api\common\model\ParamsFilterModel;
 
-class PortalCategoryModel extends ParamsFilterModel
-{
-    //类型转换
-    protected $type = [
-        'more' => 'array',
-    ];
-    //可查询字段
-    protected $visible = [
-    				'id','name','description',
-    				'seo_title','seo_keywords',
-    				'seo_description','more'
-    			];
-   	protected $relationFilter = [
-   					'articles'	=>	'hasToMany'
-   				];
-
-    /**
-     * 基础查询
-     */
-    protected function base($query)
-    {
-      $query->where('delete_time',0)
-            ->where('status',1);
-    }
-
-   	/**
-     * 关联文章表
-     * @return $this
-     */
-    public function articles()
-    {
-        return $this->belongsToMany('PortalPostModel', 'portal_category_post', 'post_id', 'category_id');
-    }
-
-	/**
-	 * more 自动转化
-	 * @param $value
-	 * @return array
-	 */
-	public function getMoreAttr($value)
+	class PortalCategoryModel extends ParamsFilterModel
 	{
-		$more = json_decode($value,true);
-		if (!empty($more['thumbnail'])) {
-			$more['thumbnail'] = cmf_get_image_preview_url($more['thumbnail']);
+		//类型转换
+		protected $type = [
+			'more' => 'array',
+		];
+		//可查询字段
+		protected $visible = [
+			'id','name','description',
+			'seo_title','seo_keywords',
+			'seo_description','more'
+		];
+		protected $relationFilter = [
+			'articles'	=>	'hasToMany'
+		];
+
+		/**
+		 * 基础查询
+		 */
+		protected function base($query)
+		{
+			$query->where('delete_time',0)
+				->where('status',1);
 		}
 
-		if (!empty($more['photos'])) {
-			foreach ($more['photos'] as $key => $value) {
-				$more['photos'][$key]['url'] = cmf_get_image_preview_url($value['url']);
-			}
+		/**
+		 * 关联文章表
+		 * @return $this
+		 */
+		public function articles()
+		{
+			return $this->belongsToMany('PortalPostModel', 'portal_category_post', 'post_id', 'category_id');
 		}
 
-		if (!empty($more['files'])) {
-			foreach ($more['files'] as $key => $value) {
-				$more['files'][$key]['url'] = cmf_get_image_preview_url($value['url']);
+		/**
+		 * more 自动转化
+		 * @param $value
+		 * @return array
+		 */
+		public function getMoreAttr($value)
+		{
+			$more = json_decode($value,true);
+			if (!empty($more['thumbnail'])) {
+				$more['thumbnail'] = cmf_get_image_preview_url($more['thumbnail']);
 			}
+
+			if (!empty($more['photos'])) {
+				foreach ($more['photos'] as $key => $value) {
+					$more['photos'][$key]['url'] = cmf_get_image_preview_url($value['url']);
+				}
+			}
+
+			if (!empty($more['files'])) {
+				foreach ($more['files'] as $key => $value) {
+					$more['files'][$key]['url'] = cmf_get_image_preview_url($value['url']);
+				}
+			}
+			return $more;
 		}
-		return $more;
 	}
-}
--- api/portal/model/PortalPostModel.php
+++ api/portal/model/PortalPostModel.php
@@ -1,69 +1,69 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: pl125 <xskjs888@163.com>
-// +----------------------------------------------------------------------
+	// +----------------------------------------------------------------------
+	// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+	// +----------------------------------------------------------------------
+	// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+	// +----------------------------------------------------------------------
+	// | Author: pl125 <xskjs888@163.com>
+	// +----------------------------------------------------------------------
 
-namespace api\portal\model;
+	namespace api\portal\model;
 
-use api\common\model\ParamsFilterModel;
+	use api\common\model\ParamsFilterModel;
 
-class PortalPostModel extends ParamsFilterModel
-{
-    //可查询字段
-    protected $visible = [
-    				'id','post_type','comment_status','is_top','recommended',
-    				'post_hits','post_like','comment_count','published_time',
-    				'post_title','post_keywords','post_excerpt','post_source',
-    				'post_content','more'
-    			];
+	class PortalPostModel extends ParamsFilterModel
+	{
+		//可查询字段
+		protected $visible = [
+			'id','post_type','comment_status','is_top','recommended',
+			'post_hits','post_like','comment_count','published_time',
+			'post_title','post_keywords','post_excerpt','post_source',
+			'post_content','more'
+		];
 
-   	/**
-   	 * 基础查询
-   	 */
-   	protected function base($query)
-   	{
-   		$query->where('delete_time',0)
-   			  ->where('post_status',1)
-   			  ->whereTime('published_time','between',[1,time()]);
-   	}
+		/**
+		 * 基础查询
+		 */
+		protected function base($query)
+		{
+			$query->where('delete_time',0)
+				->where('post_status',1)
+				->whereTime('published_time','between',[1,time()]);
+		}
 
-   	/**
-     * post_content 自动转化
-     * @param $value
-     * @return string
-     */
-    public function getPostContentAttr($value)
-    {
-        return cmf_replace_content_file_url(htmlspecialchars_decode($value));
-    }
+		/**
+		 * post_content 自动转化
+		 * @param $value
+		 * @return string
+		 */
+		public function getPostContentAttr($value)
+		{
+			return cmf_replace_content_file_url(htmlspecialchars_decode($value));
+		}
 
-    /**
-     * more 自动转化
-     * @param $value
-     * @return array
-     */
-    public function getMoreAttr($value)
-    {
-    	$more = json_decode($value,true);
-    	if (!empty($more['thumbnail'])) {
-    		$more['thumbnail'] = cmf_get_image_preview_url($more['thumbnail']);
-    	}
+		/**
+		 * more 自动转化
+		 * @param $value
+		 * @return array
+		 */
+		public function getMoreAttr($value)
+		{
+			$more = json_decode($value,true);
+			if (!empty($more['thumbnail'])) {
+				$more['thumbnail'] = cmf_get_image_preview_url($more['thumbnail']);
+			}
 
-    	if (!empty($more['photos'])) {
-    		foreach ($more['photos'] as $key => $value) {
-    			$more['photos'][$key]['url'] = cmf_get_image_preview_url($value['url']);
-    		}
-    	}
+			if (!empty($more['photos'])) {
+				foreach ($more['photos'] as $key => $value) {
+					$more['photos'][$key]['url'] = cmf_get_image_preview_url($value['url']);
+				}
+			}
 
-    	if (!empty($more['files'])) {
-    		foreach ($more['files'] as $key => $value) {
-    			$more['files'][$key]['url'] = cmf_get_image_preview_url($value['url']);
-    		}
-    	}
-    	return $more;
-    }
-}
+			if (!empty($more['files'])) {
+				foreach ($more['files'] as $key => $value) {
+					$more['files'][$key]['url'] = cmf_get_image_preview_url($value['url']);
+				}
+			}
+			return $more;
+		}
+	}
