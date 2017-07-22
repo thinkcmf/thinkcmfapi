a7ce35f7ffe9e4cdcc34f4dd5c268d2d20b95a04
wuwu <15093565100@163.com>
2017-07-22 20:07:49 +0800

    优化

--- api/portal/controller/ListsController.php
+++ api/portal/controller/ListsController.php
@@ -8,39 +8,37 @@
 // +----------------------------------------------------------------------
 namespace api\portal\controller;
 
-use cmf\controller\RestBaseController;
 use api\portal\model\PortalCategoryModel as PortalCategory;
 use api\portal\model\PortalPostModel as PortalPost;
-use think\exception\HttpResponseException;
-use think\Response;
+use cmf\controller\RestBaseController;
 
 class ListsController extends RestBaseController
 {
 
     /**
-     * [getRecommendedList 推荐列表]
+     * [getRecommendedLists 推荐列表]
      * @Author:   wuwu<15093565100@163.com>
      * @DateTime: 2017-07-17T11:36:51+0800
      * @since:    1.0
      * @return    [type]                   [json数据]
      */
-    public function getRecommendedList()
+    public function getRecommendedLists()
     {
-        $num     = $this->request->has('num') ? $this->request->param('num') : 10;
-        $next_id = $this->request->has('next_id') ? $this->request->param('next_id') : 0;
-        $list    = PortalPost::recommendedList($next_id, $num);
-        $limit   = [$next_id, $num];
-        $this->success('ok', $list, $limit);
+        $num           = $this->request->has('num') ? $this->request->param('num') : 10;
+        $next_id       = $this->request->has('next_id') ? $this->request->param('next_id') : 0;
+        $list['datas'] = PortalPost::recommendedList($next_id, $num);
+        $list['limit'] = [$next_id, $num];
+        $this->success('ok', $list);
     }
 
     /**
-     * [getCategoryPostList 分类文章列表]
+     * [getCategoryPostLists 分类文章列表]
      * @Author:    wuwu<15093565100@163.com>
      * @DateTime: 2017-07-17T15:22:41+0800
      * @since:    1.0
      * @return    [type]                   [description]
      */
-    public function getCategoryPostList()
+    public function getCategoryPostLists()
     {
         $num         = $this->request->has('num') ? $this->request->param('num') : 10;
         $next_id     = $this->request->has('next_id') ? $this->request->param('next_id') : 0;
@@ -49,32 +47,9 @@ class ListsController extends RestBaseController
         if (!PortalCategory::where('id', $category_id)->find()) {
             $this->error('fail');
         }
-        $list  = PortalPost::categoryPostList($category_id, $next_id, $num);
-        $limit = [$next_id, $num];
-        $this->success('ok', $list, $limit);
-    }
-
-    /**
-     * 操作成功跳转的快捷方法
-     * @access protected
-     * @param mixed $msg 提示信息
-     * @param mixed $data 返回的数据
-     * @param array $header 发送的Header信息
-     * @return void
-     */
-    protected function success($msg = '', $data = '', array $limit = [], array $header = [])
-    {
-        $code   = 1;
-        $result = [
-            'code'  => $code,
-            'msg'   => $msg,
-            'limit' => $limit,
-            'data'  => $data,
-        ];
-
-        $type     = $this->getResponseType();
-        $response = Response::create($result, $type)->header($header);
-        throw new HttpResponseException($response);
+        $list['datas'] = PortalPost::categoryPostList($category_id, $next_id, $num);
+        $list['limit'] = [$next_id, $num];
+        $this->success('ok', $list);
     }
 
 }
