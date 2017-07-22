9732421acf448fe1f994017c34131d112514a211
zxxjjforever <zxxjjforever@163.com>
2017-07-22 11:31:30 +0800

    优化

---
+++ api/portal/route.php
@@ -0,0 +1,7 @@
+<?php
+
+use think\Route;
+
+Route::resource('portal/categories', 'portal/Categories');
+Route::resource('portal/articles', 'portal/Articles');
+Route::resource('portal/pages', 'portal/Pages');
--- api/route.php
+++ api/route.php
@@ -19,10 +19,4 @@ foreach ($apps as $app) {
 
 
 return [
-    '__rest__' => [
-        'portal/categorys' => ['portal/categorys', ['only' => ['index', 'read']]],
-        'portal/articles'  => ['portal/articles', ['only' => ['index', 'read']]],
-        'portal/comments'  => ['portal/comments', ['only' => ['index']]],
-        'portal/pages'     => ['portal/pages', ['only' => ['index', 'read']]]
-    ]
 ];
