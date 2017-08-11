632a9ebd7a556e9f9fd5027ff2da030729d27f9a
wuwu <15093565100@163.com>
2017-08-11 21:49:03 +0800

    收藏路由优化调整

--- api/user/route.php
+++ api/user/route.php
@@ -1,6 +1,30 @@
 <?php
-
+// +----------------------------------------------------------------------
+// | 文件说明：路由
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017 http://www.wuwuseo.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: thinkcmf
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Date: 2017-8-11
+// +----------------------------------------------------------------------
 use think\Route;
 
-Route::resource('user/articles', 'user/Articles');
-Route::post('user/articles/deletes','user/Articles/deletes');
+Route::get([
+    'user/favorites' => 'user/favorites/getFavorites', //获取收藏列表
+]);
+
+Route::post([
+    'user/articles/deletes' => 'user/Articles/deletes',
+    'user/favorites' => 'user/favorites/setFavorites',//添加收藏
+]);
+
+Route::put([
+
+]);
+
+Route::delete([
+	'user/favorites/:id'=>'user/favorites/unsetFavorites',//删除收藏
+]);
