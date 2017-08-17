b0bde3f90f44a990f88c6952aaee046d72321473
王磊 <15093565100@163.com>
2017-08-17 21:12:04 +0800

    修复

--- api/portal/model/PortalCategoryModel.php
+++ api/portal/model/PortalCategoryModel.php
@@ -87,7 +87,6 @@ class PortalCategoryModel extends CommonModel
      */
     public static function categoryPostIds($category_id)
     {
-        $ids      = '';
         $post_ids = self::relation('PostIds')->field(true)->where('id', $category_id)->find();
         foreach ($post_ids['PostIds'] as $key => $id) {
             $ids[] = $id['post_id'];
