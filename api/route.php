aa4c1dc62b3453c9c404392c13cbed454358d19b
zxxjjforever <zxxjjforever@163.com>
2018-06-07 18:03:02 +0800

    修复不加载动态路由

--- api/route.php
+++ api/route.php
@@ -18,5 +18,10 @@ foreach ($apps as $app) {
 }
 
 
-return [
-];
+if (file_exists(CMF_ROOT . "data/conf/route.php")) {
+    $runtimeRoutes = include CMF_ROOT . "data/conf/route.php";
+} else {
+    $runtimeRoutes = [];
+}
+
+return $runtimeRoutes;
